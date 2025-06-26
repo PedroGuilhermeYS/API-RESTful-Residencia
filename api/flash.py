d3014819ae50c4f347c7809c83c85dae2655d96d
PedroGuilhermeYS <pedro210905@gmail.com>
2025-06-25 23:49:32 -0300

    implementação do endpoint

--- api/flash.py
+++ api/flash.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+usuarios = []
 
 app = Flask(__name__)
 
@@ -9,6 +10,20 @@ def home():
 @app.route('/saudacao', methods=['GET'])
 def saudacao():
     return jsonify ({"mensagem": "Bem-vindo à API de exemplo"})
+
+@app.route("/usuarios", methods=['POST'])
+def adicionar_usuario():
+    data = request.get_json()
+    nome = data.get('nome')
+    email = data.get('email')
+
+    if not nome or not email:
+        return jsonify({"erro": "Campos 'nome' e 'email' são obrigatórios"}), 400
+
+    novo_usuario = {"nome": nome, "email": email}
+    usuarios.append(novo_usuario)
+
+    return jsonify(novo_usuario), 201
 
 if __name__ == "__main__":
     app.run(port=3000)
